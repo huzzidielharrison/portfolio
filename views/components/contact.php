d034fb45f0ff8a0c50a86d92773202be963614f9
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-12-17 01:15:27 -0600

    fixes responsive

--- views/components/contact.php
+++ views/components/contact.php
@@ -4,12 +4,10 @@ use helpers\Svg;
 
 ?>
 
-<div class="contact">
+
+<div class="contact f-center-xy">
     <div class="grid-container">
         <div class="grid-x grid-margin-x">
-            <div class="cell">
-                <h2 class="heading h2">Contacto</h2>
-            </div>
             <div class="cell medium-4 social-link">
                 <a href="mailto:huzzidielharrison@gmail.com" target="_blank">
                     <?php Svg::render('icon-email', '', '', 'assets/images/icons/') ?>
